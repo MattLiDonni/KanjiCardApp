403b46d6d43c36a70f5cec939211407365fdb260
MattLiDonni <m.lidonni16@gmail.com>
2024-05-13 12:04:52 -0400

    Changed comment for clarity

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kanjiapp import App
 
 if __name__ == "__main__":
     if not os.getenv('DISPLAY'): 
-        # prevents pyautogui errors in github actions, normally set by your OS
+        # Supposed to prevent pyautogui errors in github actions, but doesnt. 
+        # is normally set by your OS. Needs to be fixed.
         os.environ['DISPLAY'] = ':0'
     App().start()

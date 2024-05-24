0c01d091d76dadb2a7fc0d3d1b0eb8d879b7e06a
MattLiDonni <m.lidonni16@gmail.com>
2024-05-24 14:00:02 -0400

    Removed env var from main

--- main.py
+++ main.py
@@ -3,8 +3,4 @@ import os
 from kanjiapp import App
 
 if __name__ == "__main__":
-    if not os.getenv('DISPLAY'): 
-        # Supposed to prevent pyautogui errors in github actions, but doesnt. 
-        # is normally set by your OS. Needs to be fixed.
-        os.environ['DISPLAY'] = ':0'
     App().start()

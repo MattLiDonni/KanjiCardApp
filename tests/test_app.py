1063462ea6aadde57754b3fd3d4b79cc43a2f9a2
MattLiDonni <m.lidonni16@gmail.com>
2024-05-24 13:57:33 -0400

    began implementing pyvirtualdisplay

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
+from pyvirtualdisplay import Display
 from kanjiapp import App
 
+def test_virtual_display():
+    with Display(visible=False, size=(100, 60)) as disp:
+        assert disp.is_alive()
+
 def test_app_init():
-    assert App()
+    with Display(visible=False, size=(100, 60)) as disp:
+        assert App()

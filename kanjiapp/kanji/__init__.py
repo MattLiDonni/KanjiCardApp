1c9d5fa6fba278256a090bca520f90a77b27786d
MattLiDonni <m.lidonni16@gmail.com>
2024-05-27 13:48:29 -0400

    Kanji object changes
    No longer requires any variables at init except for the character.
    Readings are being temporarily combined as JamDict doesn't seperate the readings

--- kanjiapp/kanji/__init__.py
+++ kanjiapp/kanji/__init__.py
@@ -3,21 +3,26 @@ from typing import List
 
 class Kanji:
 
-    def __init__(self, character: str, meaning: str, onyomi: List[str], kunyomi: List[str], crop:Image=None):
+    def __init__(self, character:str, meanings:str=None, readings:List[str]=None, crop:Image=None):
         self.character = character
-        self.meaning = meaning
-        self.onyomi = onyomi
-        self.kunyomi = kunyomi
+        self.meanings = meanings
+        self.readings = readings
         self.crop = crop
-        self.croptk = ImageTk.PhotoImage(self.crop)
     
     def getImageTk(self) -> ImageTk.PhotoImage:
-        return self.croptk
+        """ Return the image as an ImageTk object """
+        if self.crop:
+            return ImageTk.PhotoImage(self.crop)
+        raise ValueError("Kanji has no defined image to pull from!")
+    
+    def __repr__(self):
+        return f"Kanji({self.character}, {self.meanings}, {self.readings}, {self.crop}"
 
 class KanjiLibrary:
 
-    kanji:List[Kanji]
+    kanji: List[Kanji] = []
 
     @classmethod
     def add(cls, kanji: Kanji):
+        """ Add a kanji to the current list """
         cls.kanji.append(kanji)
